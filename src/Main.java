b754972e4edd2bbe9f77a5df27153dd8da014dc3
THERESE07 <niyigena.therese@gmail.com>
2026-01-31 10:30:33 +0200

    feat/Sum of two numbers

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public void main(String args[]){
+        int number1 = 10;
+        int number2 = 7;
+        int sum = number1 + number2;
+        System.out.println("The sum of two numbers is: " + number1 + " + " + number2 + " = " + sum);
+    }
+}
