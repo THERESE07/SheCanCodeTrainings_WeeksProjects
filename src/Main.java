cf3e3520d2826a71dea20cf02614de08b7231f9e
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 11:59:39 +0200

    feat/week1_Exercise4_Product

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public void main(String[] args){
+        int number1 = 8;
+        int number2 = 5;
+        int product = number1 * number2;
+        System.out.println("The product of two number is : " + number1 + " * " + number2 + " = " + product);
+    }
+}
