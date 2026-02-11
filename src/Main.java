4a07ee75d738cedd90a62b4c18cddb1ae2f7fa6f
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 14:19:46 +0200

    feat/week1_Exercise9_AllPaymentPerHour

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int hours = 35;
+        int price_hour_$ = 10;
+        int pay = hours * price_hour_$;
+        System.out.println("The payment of all hours is: " + hours + " * " + price_hour_$ + " = " + pay);
+    }
+}
