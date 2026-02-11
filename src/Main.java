8236222b24423a031084c36b1f1cdfd98f677d5c
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 14:32:18 +0200

    feat/week1_Exercise10_Reassignment

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 5;
+        System.out.println("The Value of x before reassignment is: " + x);
+
+        x = 10;
+        System.out.println("The Value of x after reassignment is: " + x);
+    }
+}
