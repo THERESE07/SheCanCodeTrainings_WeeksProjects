0423e5cf614390724b9755342f3157fbad33a90e
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 14:02:50 +0200

    feat/week1_Exercise8_Substraction_Difference

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int number1 = 30;
+        int number2 = 12;
+        int diff = number1 - number2;
+        System.out.println("The difference of two numbers is: " + number1 + " - " +  number2 + " = " + diff);
+    }
+}
