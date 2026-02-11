c639c2a1eec866d826cca22f7dca3903789b7103
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 12:24:24 +0200

    feat/week1_Exercise_Remainder

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public  class Main{
+    public static void main(String[] args){
+        int number1 = 20;
+        int number2 = 6;
+        int remainder = number1 % number2;
+        System.out.println("The remainder of two numbers is: " + number1 + " % " + number2 + " = " + remainder);
+    }
+}
