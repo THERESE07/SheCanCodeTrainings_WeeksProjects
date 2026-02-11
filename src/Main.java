d98f7d05425c53b7ee9da41cb1a1327470f5c657
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 12:13:05 +0200

    feat/week1_Exercise5_Quotient

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public void main(String[] args){
+        int  number1 = 15;
+        int number2 = 4;
+        int quotient = number1 / number2;
+        System.out.println("The quotient of two numbers is: " + number1 + " / " + number2 + " = " + quotient);
+    }
+}
