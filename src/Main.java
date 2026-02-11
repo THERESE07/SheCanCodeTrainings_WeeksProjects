502810f785e6a208936751a7cc46fbf83d218144
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 13:44:41 +0200

    feat/week1_Exercise7_Cost_Widening_Automatic_TypeCasting

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public  static void main(String[] args){
+        int numberOfGoods= 3;
+        double price = 4.99;
+        double cost = numberOfGoods * price;
+        System.out.println("The cost these goods is: " + numberOfGoods + " * " + price + " = " + cost);
+    }
+}
