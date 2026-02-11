90c18dc7fa5532ffffe1f15c9f512fec61d3994b
THERESE07 <niyigena.therese@gmail.com>
2026-02-11 15:03:49 +0200

    feat/week1_Exercise11_TotalOfTwoDecimalNumbers

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        double decimalNumber1 = 3.5;
+        double decimalNumber2 = 2.7;
+        double total = decimalNumber1 + decimalNumber2;
+        System.out.println("The total of two decimal numbers is: " + decimalNumber1 + " + " + decimalNumber2 + " = " + total);
+    }
+}
