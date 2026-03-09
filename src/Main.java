7239df158775d99e17733ecc11b3b477f752c10f
THERESE07 <niyigena.therese@gmail.com>
2026-03-09 19:30:07 +0200

    feat/permitting access by age as condition

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("Do You have a parental Consent? (True or False)");
+        Boolean hasParentalConsent = scanner.nextBoolean();
+
+        if(age >= 18 || hasParentalConsent){
+            System.out.println("Access granted");
+        }
+        else{
+            System.out.println("Access Denied");
+        }
+
+        scanner.close();
     }
 }
